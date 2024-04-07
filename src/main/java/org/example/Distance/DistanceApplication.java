892238aa3f8917bfe129ba20f4e76ab04c3cef76
IvanKhilko <Vanya.hilko@gmail.com>
2024-04-08 02:04:05 +0300

    commit

--- src/main/java/org/example/Distance/DistanceApplication.java
+++ src/main/java/org/example/Distance/DistanceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DistancedataaApplication {
+public class DistanceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DistancedataaApplication.class, args);
+        SpringApplication.run(DistanceApplication.class, args);
     }
 
 }
